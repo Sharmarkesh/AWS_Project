21889cbb2784e5c4ea3ddfa397250e8b52d3ef37
Sharmarkesh <sharmarkesh@outlook.com>
2026-08-26 19:51:20 +0100

    Initial workflow

---
+++ StaticsS3Website/main.tf
@@ -0,0 +1,46 @@
+#  Provider
+provider "aws" {
+  region = "eu-west-1" # change region as needed 
+}
+
+
+#  S3 Bucket
+resource "aws_s3_bucket" "static_site" {
+  bucket = "sharifBucket" # make bucket name unique
+}
+
+# S3 Remove Public Access Block
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+#  S3 Bucket Policy
+resource "aws_s3_bucket_policy" "allow_public_access" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid: "PublicReadGetObject",
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.static_site.id}/*"
+      }
+    ]
+  })
+}
+
+# S3 Bucket Website Configuration
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.static_site.id
+  index_document {
+    suffix = "index.html"
+  }
+}
