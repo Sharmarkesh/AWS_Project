2a0c1012e9310447dd3e9161a3bd17057ac11698
Sharmarkesh <sharmarkesh@outlook.com>
2026-08-26 21:01:54 +0100

    Updated main.tf

--- StaticsS3Website/main.tf
+++ StaticsS3Website/main.tf
@@ -27,10 +27,9 @@ resource "aws_s3_bucket_policy" "allow_public_access" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid: "PublicReadGetObject",
-        Effect = "Allow",
-        Principal = "*",
-        Action = ["s3:PutObject", "s3:GetObject", "s3:ListBucket"],
+       Sid      = "UploadObjects",
+        Effect   = "Allow",
+        Action   = ["s3:PutObject", "s3:GetObject"],
         Resource = "arn:aws:s3:::${aws_s3_bucket.static_site.id}/*"
       }
     ]
